2e36369b712e5c6fc8f8225098fb3c5993bf8b28
unknown <ceoDoctor@outlook.com>
2018-11-26 23:11:58 +0800

    26/11 upload mail template php file

---
+++ config/mail-templates.php
@@ -0,0 +1,142 @@
+<?php
+
+return [
+
+    'cn' => array(
+        'created_booking' => array(
+            'subject' => '通知',
+            'variables' => array(
+                array(
+                    'name' => 'find_address',
+                    'content' => '我们的地址您找不到?'
+                ),
+                array(
+                    'name' => 'click_here',
+                    'content' => '请点击此处'
+                ),
+                array(
+                    'name' => 'contact_with_us',
+                    'content' => '如果您想编辑或取消预订，请联系我们。'
+                ),
+                array(
+                    'name' => 'special_requests',
+                    'content' => '特殊要求和忌口食物'
+                ),
+                array(
+                    'name' => 'no_requests',
+                    'content' => '无特殊要求'
+                ),
+                array(
+                    'name' => 'people',
+                    'content' => '人'
+                ),
+                array(
+                    'name' => 'address_outer_link',
+                    'content' => 'http://fccworld.cn/'
+                ),
+                array(
+                    'name' => 'contact_phone',
+                    'content' => '(+86) 18621672399'
+                ),
+                array(
+                    'name' => 'contact_email',
+                    'content' => 'reservations@fccworld.cn'
+                )
+            )
+        ),
+        'created_booking_admin' => array(
+            'subject' => '通知',
+            'variables' => array(
+                array(
+                    'name' => 'no_requests',
+                    'content' => '无特殊要求'
+                ),
+                array(
+                    'name' => 'people',
+                    'content' => '人'
+                ),
+                array(
+                    'name' => 'open_dashboard',
+                    'content' => '控制板'
+                ),
+                array(
+                    'name' => 'dashboard_link',
+                    'content' => 'http://admin.fccworld.cn/'
+                ),
+                array(
+                    'name' => 'COMPANY',
+                    'content' => 'SHANGHAI FCC FOOD & BEVERAGE CO., LTD.'
+                )
+            )
+        )
+    ),
+
+    'en' => array(
+        'created_booking' => array(
+            'subject' => 'Notification',
+            'variables' => array(
+                array(
+                    'name' => 'find_address',
+                    'content' => "You can't find our address?"
+                ),
+                array(
+                    'name' => 'click_here',
+                    'content' => 'Click here'
+                ),
+                array(
+                    'name' => 'contact_with_us',
+                    'content' => 'If you wish to edit or cancel your reservation please get in contact with us.'
+                ),
+                array(
+                    'name' => 'special_requests',
+                    'content' => 'Special requests'
+                ),
+                array(
+                    'name' => 'no_requests',
+                    'content' => 'No special request'
+                ),
+                array(
+                    'name' => 'people',
+                    'content' => 'People'
+                ),
+                array(
+                    'name' => 'address_outer_link',
+                    'content' => 'http://fccworld.cn/'
+                ),
+                array(
+                    'name' => 'contact_phone',
+                    'content' => '(+86) 18621672399'
+                ),
+                array(
+                    'name' => 'contact_email',
+                    'content' => 'reservations@fccworld.cn'
+                )
+            )
+        ),
+        'created_booking_admin' => array(
+            'subject' => 'Notification',
+            'variables' => array(
+                array(
+                    'name' => 'no_requests',
+                    'content' => 'No special request'
+                ),
+                array(
+                    'name' => 'people',
+                    'content' => 'People'
+                ),
+                array(
+                    'name' => 'open_dashboard',
+                    'content' => 'Dashboard'
+                ),
+                array(
+                    'name' => 'dashboard_link',
+                    'content' => 'http://admin.fccworld.cn/'
+                ),
+                array(
+                    'name' => 'COMPANY',
+                    'content' => 'SHANGHAI FCC FOOD & BEVERAGE CO., LTD.'
+                )
+            )
+        )
+    )
+];
